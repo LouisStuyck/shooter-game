17a9921c3e08b277a25b7e548c506c005efa108c
LouisStuyck <stuycklouis@gmail.com>
2018-11-05 18:02:51 +0100

    :sparkles: Add shootTarget

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ shooter.$start = shooter.$container.querySelector('.start')
 shooter.$score = shooter.$container.querySelector('.score .value')
 shooter.$timer = shooter.$container.querySelector('.timer')
 shooter.$targets = shooter.$container.querySelector('.targets')
+shooter.score = 0
 
 /**
  * Methods
@@ -19,6 +20,26 @@ shooter.addTarget = () =>
     $target.style.top = `${Math.random() * 100}%`
     $target.style.left = `${Math.random() * 100}%`
     shooter.$targets.appendChild($target)
+
+    $target.addEventListener('mouseenter', () =>
+    {
+        shooter.shootTarger($target)
+    })
+}
+
+shooter.shootTarger = (_$target) =>
+{
+    //delete element
+    _$target.remove()
+
+    //add new target
+    shooter.addTarget()
+
+    //Increment score
+    shooter.score++
+    shooter.$score.textContent = shooter.score
+
+    //play sound
 }
 
 shooter.addTarget()
